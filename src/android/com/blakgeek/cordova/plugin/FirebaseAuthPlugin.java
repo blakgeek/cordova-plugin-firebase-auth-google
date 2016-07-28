338a0118f5c83a90b2c6d07c82a268599fc393de
Carlos Lawton <blakgeek@gmail.com>
2016-07-28 07:07:24 -0400

    Completed support for sign out.

--- src/android/com/blakgeek/cordova/plugin/FirebaseAuthPlugin.java
+++ src/android/com/blakgeek/cordova/plugin/FirebaseAuthPlugin.java
@@ -1,24 +1,17 @@
 package com.blakgeek.cordova.plugin;
 
 // IMPORT_R
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
