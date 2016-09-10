061085e88968822e9e00170609e554230c5bc313
Carlos Lawton <blakgeek@gmail.com>
2016-09-09 22:44:37 -0400

    moved AppDelegate into it's own files

--- src/ios/FirebaseAuthPlugin.m
+++ src/ios/FirebaseAuthPlugin.m
@@ -3,45 +3,13 @@
 #import "objc/runtime.h"
 @import Firebase;
 
-static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector);
-
-@implementation AppDelegate (FoSwizzle)
-
-+ (void)load {
-
-    swizzleMethod([AppDelegate class],
-            @selector(application:openURL:options:),
-            @selector(identity_application:openURL:options:));
-    swizzleMethod([AppDelegate class],
-            @selector(application:openURL:sourceApplication:annotation:),
-            @selector(identity_application:openURL:sourceApplication:annotation:));
-}
-
-#pragma mark - AppDelegate Swizzles
-
-- (BOOL)identity_application:(UIApplication *)app
-                     openURL:(NSURL *)url
-                     options:(NSDictionary<NSString *, id> *)options {
-    return [[GIDSignIn sharedInstance] handleURL:url
-                               sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                                      annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
-}
-
-- (BOOL)identity_application:(UIApplication *)application
-                     openURL:(NSURL *)url
-           sourceApplication:(NSString *)sourceApplication
-                  annotation:(id)annotation {
-    return [[GIDSignIn sharedInstance] handleURL:url
-                               sourceApplication:sourceApplication
-                                      annotation:annotation];
-}
-@end
-
 @implementation FirebaseAuthPlugin
 
 - (void)initialize:(CDVInvokedUrlCommand *)command {
-
-    [FIRApp configure];
+    
+    if(![FIRApp defaultApp]) {
+        [FIRApp configure];
+    }
     [GIDSignIn sharedInstance].clientID = [FIRApp defaultApp].options.clientID;
     [GIDSignIn sharedInstance].uiDelegate = self.viewController;
     [GIDSignIn sharedInstance].delegate = self;
@@ -104,6 +72,7 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{
                 @"type" : @"signinfailure",
                 @"data" : @{
+
                         @"code" : [NSNumber numberWithInteger:error.code],
                         @"message" : error.description
                 }
@@ -169,17 +138,4 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.eventCallbackId];
 }
 
-
 @end
-
-static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector) {
-    Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
-    Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
-
-    // If the method doesn't exist, add it.  If it does exist, replace it with the given implementation.
-    if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
-        class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
-    } else {
-        method_exchangeImplementations(destinationMethod, sourceMethod);
-    }
-}
